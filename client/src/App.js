1bcc12f25a6e331db9f0a4f87436f44068c8b271
Fernando Barron <fernando_barsan@hotmail.com>
2020-08-01 19:56:37 -0500

    Quitar Other Page no jala

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import OtherPage from './OtherPage';
+//import OtherPage from './OtherPage';
 import Fib from './Fib';
 
 function App() {
@@ -13,11 +13,9 @@ function App() {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
           <Link to="/">Home</Link>
-          <Link to="/otherpage">Other Page</Link>
         </header>
         <div>
           <Route exact path="/" component={Fib} />
-          <Route exact path="/otherpage" component={OtherPage} />
         </div>
       </div>
     </Router>
